50520d0242bad64b6c6b7c678215a56ed89f3c7a
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-09-15 11:28:13 +0630

    [3526][ADD] mrp_view_adj (#2)

---
+++ mrp_view_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Quartile Limited (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP View Adj",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["mrp"],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True,
+}
